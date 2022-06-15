c7e8c197270a7bada636b9938cde690d000005d1
jusong23 <jusong23@naver.com>
2022-06-15 10:43:22 +0900

    boolean형

--- char.cpp
+++ char.cpp
@@ -5,11 +5,16 @@ using namespace std;
 int main()
 {
 
-    int a = 77;
-    char b = 'a'; // 작은 문자형 하나만 사용할 수 있음 , 그래서 ''이용하고 null값 따로 표현해야 함
+    int as = 77;
+    char bc = 'a'; // 작은 문자형 하나만 사용할 수 있음 , 그래서 ''이용하고 null값 따로 표현해야 함
     // null 문자
-    char c[] = {'a', 'b', '\0'};
+    char ff[] = {'a', 'b', '\0'};
 
-    string d = "a"; // string은 문자열, 즉 여러문자 표현가능 하므로, 큰 따옴표안에 한개이상의 문자, a와 null 을 담을 수 있는 것
-    cout << d << endl;
+    string df = "a"; // string은 문자열, 즉 여러문자 표현가능 하므로, 큰 따옴표안에 한개이상의 문자, a와 null 을 담을 수 있는 것
+
+    bool a = 0;
+    bool b = 2;
+    bool c = 23;
+
+    cout << a << " " << b << " " << c << endl;
 }
