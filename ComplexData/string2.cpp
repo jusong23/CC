f64c8ca362861919c51eaa5e46c1d5cbc738ec5a
jusong23 <jusong23@naver.com>
2022-06-23 10:14:44 +0900

    string2

---
+++ ComplexData/string2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char char1[10];
+    char char2[10] = "jager";
+    string str1;
+    string str2 = "panda";
+
+    str1 = str2;
+    cout << str1[0] << endl;
+}
