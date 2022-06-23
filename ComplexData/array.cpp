973431a40ca3f06e0bed4a9f51355bdaf78d12a5
jusong23 <jusong23@naver.com>
2022-06-23 09:33:58 +0900

    배열과 문자열

---
+++ ComplexData/array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // short month[12] = {1, 2, 3};
+    // cout << month[4] << endl;
+
+    int finger[] = {1, 2, 3};
+    cout << finger[1] << endl;
+
+    char a[] = {'H', 'I'};
+    char b[] = "Hello";
+    // 배열 사이즈 비워주면 알아서 사이즈 정해주므로 명시적으로 "" 이용해서 할당하기
+    return 0;
+}
